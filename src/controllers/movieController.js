fccc23ab5eee627c699389694e58ca659f1c4e8d
MatheusFreitas <moncaomatheus@gmail.com>
2024-11-05 14:17:59 -0300

    Fixed variable naming and added newline at end of file in movieController and movieRepository

--- src/controllers/movieController.js
+++ src/controllers/movieController.js
@@ -5,8 +5,8 @@ const movieRepository = new MovieRepository()
 class movieController {
     async getAllMovies(req, res) {
         try {
-            const movie = await movieRepository.getAllMovies();
-            res.json(movie);
+            const movies = await movieRepository.getAllMovies();
+            res.json(movies);
         } catch (error) {
             res.status(500).json({ error: error.message });
         }
--- src/repositories/movieRepository.js
+++ src/repositories/movieRepository.js
@@ -1,14 +1,15 @@
-const  Movies  = require('../models/movie');
+const Movies = require('../models/movie');
 
 class MovieRepository {
 
     async getAllMovies() {
         try {
-            return await Movies.findAll()
+            return await Movies.findAll() 
         } catch (error) {
             throw new Error(`Erro ao buscar todos os filmes: ${error.message}`);
         }
     }
+
 }
 
 module.exports = MovieRepository;
