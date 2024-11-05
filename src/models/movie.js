3674972308b2714150ce08b9ad7a6db003d4a275
MatheusFreitas <moncaomatheus@gmail.com>
2024-11-05 14:06:07 -0300

    Updated Sequelize import and file path in database and movie models

--- src/models/movie.js
+++ src/models/movie.js
@@ -1,5 +1,5 @@
 const { DataTypes } = require('sequelize');
-const sequelize = require('../config/database.js');
+const sequelize = require('../config/database');
 
 
 const Movies = sequelize.define('Movies', {
