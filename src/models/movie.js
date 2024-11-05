c321cfb2757cf6a2d151a8b3aea434fb3843b2ad
MatheusFreitas <moncaomatheus@gmail.com>
2024-11-05 14:26:02 -0300

    Remove empty line in movie model definition.

--- src/models/movie.js
+++ src/models/movie.js
@@ -1,7 +1,6 @@
 const { DataTypes } = require('sequelize');
 const sequelize = require('../config/database');
 
-
 const Movies = sequelize.define('Movies', {
     title: {
         type: DataTypes.STRING,
