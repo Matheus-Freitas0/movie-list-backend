a8dc8721c3f486e8d700821a2382cf8697301f74
Matheus-Freitas0 <moncaomatheus@gmail.com>
2024-11-12 19:14:55 -0300

    Updated package-lock.json and refactored config.js and models to use Sequelize

--- src/config.js
+++ src/config.js
@@ -1,9 +1,15 @@
 require('dotenv').config();
+const { Sequelize } = require('sequelize');
 
-module.exports = {
-  dialect: 'mysql',
-  host: process.env.DB_HOST,
-  username: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME,
-};
+const sequelize = new Sequelize(
+  process.env.DB_NAME,
+  process.env.DB_USER,
+  process.env.DB_PASSWORD,
+  {
+    host: process.env.DB_HOST,
+    dialect: 'mysql',
+  }
+);
+console.log("DB_USER:", process.env.DB_USER);
+console.log("DB_PASSWORD:", process.env.DB_PASSWORD);
+module.exports = sequelize;
